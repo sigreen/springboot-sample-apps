f973cdc8b224c6b4655362ee42ec90985dfa04a3
Erfin Feluzy <efeluzy@redhat.com>
2020-09-30 11:57:24 +0700

    add h2

---
+++ src/test/java/com/erfinfeluzy/sbmssql/SbMssqlApplicationTests.java
@@ -0,0 +1,9 @@
+package com.erfinfeluzy.sbmssql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbMssqlApplicationTests {
+
+}
